0eab45c2051676999b8e969679048ab2974c5a3f
Bizoon <marcinbaron93@gmail.com>
2019-01-29 12:26:15 +0100

    komentarz2

--- tasker/models.py
+++ tasker/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    name = models.CharField(max_length=72)
+    created = models.DateTimeField(auto_now_add=True)
 
 class User(models.Model):
     name = models.CharField(max_length=72)
